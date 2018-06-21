2ce9310582984b7f42e57ef090d0606785b11345
zhengjiajin <zhengjiajin@jiajin>
2018-06-21 10:54:05 +0800

    1

---
+++ swift-util/src/main/java/com/swift/util/http/HttpClient.java
@@ -0,0 +1,15 @@
+/*
+ * @(#)HttpClient.java   1.0  2018年6月21日
+ * 
+ * Copyright (c)	2014-2020. All Rights Reserved.	GuangZhou hhmk Technology Company LTD.
+ */
+package com.swift.util.http;
+
+/**
+ * 添加说明 
+ * @author zhengjiajin
+ * @version 1.0 2018年6月21日
+ */
+public class HttpClient {
+
+}
