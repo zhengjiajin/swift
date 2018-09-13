b845fb5db5d4bc28dab94c5a4a8dafe67fbb55f8
zhengjiajin <zhengjiajin@jiajin>
2018-09-13 14:31:22 +0800

    1

--- swift-util/src/main/java/com/swift/util/layout/EmojiCleanUtil.java
+++ swift-util/src/main/java/com/swift/util/layout/EmojiCleanUtil.java
@@ -12,9 +12,6 @@ package com.swift.util.layout;
  * @version 1.0 2015年8月29日
  */
 public class EmojiCleanUtil {
-    
-    public static final String EMOJI_URLENCODE="EMOJI_URLENCODE=";
-    
     /**
      * Emoji String
      * @param source
