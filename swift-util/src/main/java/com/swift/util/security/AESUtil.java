08e8bbc19611dbb2331a55ee7f675c4631e1dbf3
zhengjiajin <zhengjiajin@jiajin>
2018-11-08 18:17:05 +0800

    1

--- swift-util/src/main/java/com/swift/util/security/AESUtil.java
+++ swift-util/src/main/java/com/swift/util/security/AESUtil.java
@@ -4,11 +4,11 @@ import java.security.Key;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.collections.map.LRUMap;
 
 import com.swift.exception.UnknownException;
+import com.swift.util.io.ByteUtil;
 
 /**
  * This class is used for ...
@@ -21,46 +21,75 @@ import com.swift.exception.UnknownException;
  */
 public class AESUtil {
 	/**
-	 * use password to encrypt content by AES(128bit). 
+	 * 16进制字段串加解密
 	 * @param content	you want to encrypt message.
 	 * @param password	the encrypt key you want to use.
 	 * @return the content after encrypted.
 	 */
 	public static String encrypt(String content, String password){
 		try {
-			byte[] ptext = content.getBytes();
-			byte[] ctext = AESUtil.getEncryptCipher(password).doFinal(ptext);
-			return byte2hex(ctext);
+		    byte[] pwd = ByteUtil.hexToBytes(DigestUtils.md5Hex(password));
+			byte[] ctext = AESUtil.getEncryptCipher(pwd).doFinal(content.getBytes());
+			return ByteUtil.bytesToHex(ctext);
 		} catch(Exception e) {
 		    throw new UnknownException("AES异常",e);
 		}
 	}
 	/**
-	 * use password to decrypt content by AES(128bit).
+	 * 16进制字段串加解密
 	 * @param content	you want to decrypt content.
 	 * @param password  the you want to use.
 	 * @return	the content after decrypted.
 	 */
 	public static String decrypt(String content, String password) {
 		try {
-			byte[] ptext = AESUtil.getDecryptCipher(password).doFinal(hex2byte(content)); 
+		    byte[] pwd = ByteUtil.hexToBytes(DigestUtils.md5Hex(password));
+			byte[] ptext = AESUtil.getDecryptCipher(pwd).doFinal(ByteUtil.hexToBytes(content)); 
 			return new String(ptext);
 		} catch(Exception e) {
 		    throw new UnknownException("AES异常",e);
 		}
 	}
 	
+	/**
+     * use password to encrypt content by AES(128bit). 
+     * @param content   you want to encrypt message.
+     * @param password  the encrypt key you want to use.
+     * @return the content after encrypted.
+     */
+    public static byte[] encrypt(byte[] content, byte[] password){
+        try {
+            byte[] ctext = AESUtil.getEncryptCipher(password).doFinal(content);
+            return ctext;
+        } catch(Exception e) {
+            throw new UnknownException("AES异常",e);
+        }
+    }
+    /**
+     * use password to decrypt content by AES(128bit).
+     * @param content   you want to decrypt content.
+     * @param password  the you want to use.
+     * @return  the content after decrypted.
+     */
+    public static byte[] decrypt(byte[] content, byte[] password) {
+        try {
+            byte[] ptext = AESUtil.getDecryptCipher(password).doFinal(content); 
+            return ptext;
+        } catch(Exception e) {
+            throw new UnknownException("AES异常",e);
+        }
+    }
+	
 	private static final int maxSize = 100;
 	private static LRUMap cacheEncryptCipher = new LRUMap(maxSize);
 	private static LRUMap cacheDecryptCipher = new LRUMap(maxSize);
-	private static Cipher getEncryptCipher(String password) {
+	private static Cipher getEncryptCipher(byte[] password) {
 		try {
 			Cipher cp = (Cipher) cacheEncryptCipher.get(password);
 			if(cp == null) {
-				Key key = AESUtil.getKey(password);
+				Key key = new SecretKeySpec(password, "AES");
 				cp = Cipher.getInstance("AES");
 				cp.init(Cipher.ENCRYPT_MODE, key);
-				
 				cacheEncryptCipher.put(password, cp);
 			}
 			return cp;
@@ -68,14 +97,13 @@ public class AESUtil {
 		    throw new UnknownException("AES异常",e);
 		}
 	}
-	private static Cipher getDecryptCipher(String password) {
+	private static Cipher getDecryptCipher(byte[] password) {
 		try {
 			Cipher cp = (Cipher) cacheDecryptCipher.get(password);
 			if(cp == null) {
-				Key key = AESUtil.getKey(password);
+			    Key key = new SecretKeySpec(password, "AES");
 				cp = Cipher.getInstance("AES");
 				cp.init(Cipher.DECRYPT_MODE, key);
-				
 				cacheDecryptCipher.put(password, cp);
 			}
 			return cp;
@@ -84,43 +112,13 @@ public class AESUtil {
 		}
 	}
 	
-	private static Key getKey(String publickey) {
-		byte[] bytes = hex2byte(DigestUtils.md5Hex(publickey));
-		return new SecretKeySpec(bytes, "AES");
+	public static void main(String[] args) throws Exception {
+	    
+	    String key="zhangf";
+        String content = "I'am testing...";
+        String encode = AESUtil.encrypt(content, key);
+        String decode = AESUtil.decrypt(encode, key);
+        System.out.println(content+"\n"+encode+"\n"+decode);
 	}
-	public static byte[] hex2byte(String strhex) {
-		if (strhex == null) {
-			return null;
-		}
-		int l = strhex.length();
-		if (l % 2 != 0) {
-			return null;
-		}
-		byte[] b = new byte[l / 2];
-		for (int i = 0; i != l / 2; i++) {
-			b[i] = (byte) Integer.parseInt(strhex.substring(i * 2, i * 2 + 2), 16);
-		}
-		return b;
-	}
-
-	public static String byte2hex(byte b[]) {
-		StringBuilder sb = new StringBuilder();
-		for (int n = 0; n < b.length; n++) {
-			String stmp = Integer.toHexString(b[n] & 0xff);
-			if (stmp.length() == 1) {
-				sb.append("0");
-			}
-			sb.append(stmp);
-		}
-		return sb.toString().toUpperCase();
-	}
-
-	public static void main(String[] args) {
-		String key="zhangf";
-		String content = "I'am testing...";
-		String encode = AESUtil.encrypt(content, key);
-		String base64 = Base64.encodeBase64String(AESUtil.hex2byte(encode));
-		String decode = AESUtil.decrypt(encode, key);
-		System.out.println(content+"\n"+encode+"\n"+base64+"\n"+decode);
-	}
+	
 }
