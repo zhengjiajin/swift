59a40fe397c249f945ba258a0e27bc6e16c933e1
zhengjiajin <zhengjiajin@jiajin>
2019-01-04 11:35:11 +0800

    1

--- swift-util/src/main/java/com/swift/util/security/MD5Utils.java
+++ swift-util/src/main/java/com/swift/util/security/MD5Utils.java
@@ -6,51 +6,38 @@ import java.security.NoSuchAlgorithmException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.swift.util.io.ByteUtil;
+
 public class MD5Utils {
     private static final Logger log = LoggerFactory.getLogger(MD5Utils.class);
-	// 全局数组
-    private final static String[] strDigits = { "0", "1", "2", "3", "4", "5",
-            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
-
-
-    // 返回形式为数字跟字符串
-    private static String byteToArrayString(byte bByte) {
-        int iRet = bByte;
-        // System.out.println("iRet="+iRet);
-        if (iRet < 0) {
-            iRet += 256;
+	
+    /**
+     * MD5
+     * @param value
+     * @return
+     */
+    public static byte[] md5(byte[] value) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return md.digest(value);
+        } catch (NoSuchAlgorithmException ex) {
+            log.error("MD5签名失败",ex);
         }
-        int iD1 = iRet / 16;
-        int iD2 = iRet % 16;
-        return strDigits[iD1] + strDigits[iD2];
+        return null;
     }
-
-    // 返回形式只为数字
-    public static String byteToNum(byte bByte) {
-        int iRet = bByte;
-        System.out.println("iRet1=" + iRet);
-        if (iRet < 0) {
-            iRet += 256;
-        }
-        return String.valueOf(iRet);
-    }
-
-    // 转换字节数组为16进制字串
-    private static String byteToString(byte[] bByte) {
-        StringBuffer sBuffer = new StringBuffer();
-        for (int i = 0; i < bByte.length; i++) {
-            sBuffer.append(byteToArrayString(bByte[i]));
-        }
-        return sBuffer.toString();
-    }
-
+    
+    /**
+     * MD5后输出16进制小写
+     * @param strObj
+     * @return
+     */
     public static String getMD5Code(String strObj) {
         String resultString = null;
         try {
             resultString = new String(strObj);
             MessageDigest md = MessageDigest.getInstance("MD5");
             // md.digest() 该函数返回值为存放哈希值结果的byte数组
-            resultString = byteToString(md.digest(strObj.getBytes()));
+            resultString = ByteUtil.bytesToHex(md.digest(strObj.getBytes())).toLowerCase();
         } catch (NoSuchAlgorithmException ex) {
             log.error("MD5签名失败",ex);
         }
