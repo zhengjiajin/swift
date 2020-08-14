5d37f0a60ec6573ac4a4cb7fc261bf0bf9873fe1
zhengjiajin <zhengjiajin@jiajin>
2020-08-14 11:40:06 +0800

    测试工程类的BUG

--- swift-test/src/main/java/com/swift/test/BaseJunit4Test.java
+++ swift-test/src/main/java/com/swift/test/BaseJunit4Test.java
@@ -7,15 +7,18 @@ package com.swift.test;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * 
  * @author 郑家锦
  * @version 1.0 2018年1月2日
  */
-@ContextConfiguration(loader = TestContextLoader.class)
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration(loader = TestContextLoader.class,locations = {"classpath*:*.xml"})
 public class BaseJunit4Test extends AbstractJUnit4SpringContextTests {
     protected Log logger = LogFactory.getLog(this.getClass());
 }
