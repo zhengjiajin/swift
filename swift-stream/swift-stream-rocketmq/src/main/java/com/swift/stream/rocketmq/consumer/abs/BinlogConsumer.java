3980d4f538ee475c053a871f17abeab82db05105
zhengjiajin <231045347@qq.com>
2021-01-13 10:55:53 +0800

    兼容旧系统

--- swift-stream/swift-stream-rocketmq/src/main/java/com/swift/stream/rocketmq/consumer/abs/BinlogConsumer.java
+++ swift-stream/swift-stream-rocketmq/src/main/java/com/swift/stream/rocketmq/consumer/abs/BinlogConsumer.java
@@ -27,6 +27,7 @@ public abstract class BinlogConsumer implements RocketMqMessageListener {
         MqRequest req = new MqRequest();
         DataModel data = DataJsonParser.jsonToObject(new String(message.getBody()));
         req.setTag(data.getString("database")+"."+data.getString("table"));
+        req.setMethod(req.getTag());
         req.setData(data);
         return req;
     }
